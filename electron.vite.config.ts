606ba6ab8298ce65ef39bf7773b001d0e3a0fa96
hcarminati <hscarminati@hotmail.com>
2026-02-25 17:44:29 -0500

    fix(renderer): hardcode vite supabase keys in define plugin for production bundle

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -12,6 +12,10 @@ export default defineConfig({
         '@renderer': resolve('src/renderer/src')
       }
     },
-    plugins: [react()]
+    plugins: [react()],
+    define: {
+      'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(process.env.VITE_SUPABASE_URL),
+      'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(process.env.VITE_SUPABASE_ANON_KEY)
+    }
   }
 })
